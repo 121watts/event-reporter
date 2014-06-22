c5d3c4ed7a4def154e11bfc01e6b981984c62c8f
Andrew Watkins <andrew.watkinz@gmail.com>
2014-06-22 10:38:27 -0600

    fix integration test

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -8,15 +8,14 @@ class IntegrationTest < Minitest::Test
   def test_lookup_by_last_name
     event_report = EventReport.new
     entries = event_report.lookup('Nguyen').sort_by {|e| e.last_name}
-  end
 
-  assert_equal 2, entries.length
-  e1, e2 = entries
+    assert_equal 2, entries.length
+    e1, e2 = entries
 
-  assert_equal "Allison Nguyen" e1.name
-  assert_equal "20010", e1.zipcode
+    assert_equal "Allison Nguyen", e1.name
+    assert_equal "20010", e1.zipcode
 
-  assert_equal "Marta Nguyen" e1.name
-  assert_equal "23220", e1.zipcode
+    assert_equal "Marta Nguyen", e1.name
+    assert_equal "23220", e1.zipcode
   end
 end
