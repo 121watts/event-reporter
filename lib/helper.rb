35e872eac07746fea4da9a9998e9277c549f011e
Andrew Watkins <andrew.watkinz@gmail.com>
2014-06-26 10:56:03 -0600

    removes old printer refines help instructions

--- lib/helper.rb
+++ lib/helper.rb
@@ -18,25 +18,30 @@ class Helper
 
   def help_three(parts)
     case parts[3]
-    when "by" then puts "The instructions of 'queue print by'"
-    when "to" then puts "'queue save to' instructions"
+    when "by" then puts "Type 'queue print <attribute>' Attributes include: "
+                        +"\nfirst name, last name, zipcode, city, and state."
+    when "to" then puts "Type 'queue save to <filename.extension of your choice>'" +
+                        "\n to save file\n"
     else @legend.missing_command
     end
   end
 
   def help_two(parts)
     case parts[2]
-    when "clear" then puts "'Queue clear' instructions..."
-    when "print" then puts "'Queue print' instructions"
+    when "clear" then puts "Type 'queue clear' to clear current queue.\n"
+    when "print" then puts "Type 'queue print' to print current queue.\n"
+    when "count" then puts "Type 'queue count' to find out how many attendees are in your current queue search\n"
     else @legend.missing_command
     end
   end
 
   def help_one(parts)
     case parts[1]
-    when "queue" then puts "'Queue' instructions"
-    when "load" then puts "'Load' Instructions..."
-    when "find" then puts "'find' instructions..."
+    when "queue" then puts "Available commands for 'queue' are:" +
+                           "\n'print'\n'count'\n'cleart'\n'save to'" +
+                           "\n'print by'\n"
+    when "load" then puts "\ntype: load yourfilename.csv\n"
+    when "find" then puts "\njust type 'help' for all find commands'\n"
     else @legend.missing_command
     end
   end
--- lib/legends.rb
+++ lib/legends.rb
@@ -1,53 +1,64 @@
 class Legends
 
   def welcome
-    puts "Welcome to Event Reproter!!".red
+    puts "\nWelcome to Event Reporter!!".blue
   end
 
   def print_command
-    print "Type your command here: "
+    print "\nType your command here: "
   end
 
   def close_program
-    puts "Goodbye, we hope Event Reporter was helpful for you."
+    puts "\nGoodbye, we hope Event Reporter was helpful for you."
   end
 
   def need_file
-    "You need to specify a file to load."
+    "\nPlease specify a file to load."
   end
 
   def load_incomplete
-    puts "You need to specify a file to load.\nPlease try again."
+    puts "\nYou need to specify a file to load.\n" +
+         "\nFor example, type: load yourfilename.csv\n"
   end
 
   def load_no_file
-    puts "There is no such file.\nPlease try again with a real filename."
+    puts "\nThere is no such file.\nPlease try again with a real filename."
   end
 
   def missing_command
-    puts "Sorry, but this is not a valid command.\nTo know the available functions in Event Reporter, please type help."
+    puts "\nSorry, but this is not a valid command.\nTo know the available functions in Event Reporter, please type help."
   end
 
   def queue_saved
-    puts "The queue is saving the file."
+    puts "\nThe queue is saving the file."
   end
 
   def queue_wrong_command
-    puts "That command is not available on queue.\nYou can type the word 'help' for mor instructions."
+    puts "\nThat command is not available on queue.\nYou can type the word 'help' for mor instructions."
   end
 
   def queue_needs_commands
-    puts "To use queue you need to give more instructions. Please type 'help' if you need more directions."
+    puts "\nTo use queue you need to give more instructions. Please type 'help' if you need more directions."
   end
 
   def count_result(sort)
-    puts "There are #{sort.count} entries in the queue.\n\n"
+    puts "\nThere are #{sort.count} entries in the queue.\n\n"
   end
 
   def available_commands
-    puts "Welcome tho Event Reporter.\n"
-  end
+    puts "\nAVAILABLE COMMANDS:\n" +
+         "\n'load <filename>' - to load file you would like to search" +
+         "\n'queue count' - counts your current attendee search" +
+         "\n'queue clear' - clears most recent attendee search" +
+         "\n'queue print by <attribute>' - prints attendees by various attributes" +
+         "\n'queue save to <desired file name>'" +
+         "\n'find <attribute> <criteria>'\n" +
+         "\n  <attribute> can be - first name, last name, city, state, and zipcode\n" +
+         "\n  example search: find first name John\n" +
+         "\n                  find last name Watkins\n" +
+         "\n                  find state DC"
 
+  end
 
 
 
